fa3d4f66e6fb593acc77c663ce63619363089723
Charlie Crutcher <charliemarkcrutcher@gmail.com>
2025-03-24 16:50:51 +0000

    Website now automated to update with latest photographs from discord reacted to with cloud emoji

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+
+# Connects to MySQL database.
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="GotUser85",
+    database="testdatabase"
+)
+
 
 @app.route("/")
 def index():
@@ -20,7 +31,13 @@ def gamedev():
 
 @app.route("/photography")
 def photography():
-    return render_template("photography.html")
+    mycursor = mydb.cursor()
+    # Query to get image URLs
+    mycursor.execute("SELECT image_url FROM images")
+    images_data = mycursor.fetchall() # Returns a list of tuples
+    # Convert list of tuples to list of image URLs
+    image_urls = [url for (url,) in images_data]
+    return render_template("photography.html", images=image_urls)
 
 @app.route("/portfolio")
 def portfolio():
