634d5fd3d93804ea491f0f97b766889e78eb2a0c
Charlie Crutcher <charliemarkcrutcher@gmail.com>
2025-03-24 13:54:27 +0000

    simple bot added

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+TARGET_CHANNEL_ID = "Channel ID goes here"
 
-bot.run('Bot token goes in here')
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('hello'):
+            await message.channel.send(f'Hi there {message.author}!')
+            
+
+intents = discord.Intents.default()
+intents.message_content = True # Give bot access to message content.
+
+
+client = Client(intents=intents)
+client.run('Token goes here')
